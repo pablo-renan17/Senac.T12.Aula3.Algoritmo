7c0ac7487a9f8d5bedc864bd478a063d68f46a18
LoboGamingg <pablorenan12@outlook.com>
2024-03-12 19:01:31 -0300

    adicionado codigos da aula passada.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Imprime um texto no console
+Console.WriteLine("Seja bem vindo");
+Console.WriteLine("Digite seu nome:");
+//le o texto digitado no console
+var texto = Console.ReadLine();
+
+Console.WriteLine("Olá " + texto);
+//espera que o usuario aperte uma tecla ante de fechar o console
+Console.ReadKey();
